215f4120af15cda8da03078af185fc12270769d2
CUN-bjy <wnsdlqjtm@naver.com>
2017-08-14 20:53:41 +0900

    simulator server update

--- src/networks/simulator.py
+++ src/networks/simulator.py
@@ -1,18 +1,25 @@
+#!/usr/bin/env python
 import neat, visualize
 import os, pickle, sys
 import rospy
 
-def simulate(config_file):
+from walkyto.srv import *
+
+def simulate(req):
+	local_dir = os.path.dirname(__file__)
+	config_file = os.path.join(local_dir, 'config-feedforward')
+
 	config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
 						neat.DefaultSpeciesSet, neat.DefaultStagnation,
 						config_file)
 
 	# while(True):
-	gene_id = 4 #gene_id_getter()
+	gene_id = req.gene_id
 	gene_f = open('./genes/%s' % gene_id)
 	genome = pickle.load(gene_f)
 
 	net = neat.nn.FeedForwardNetwork.create(genome, config)
+	print gene_id
 		# gazebo_init()
 
 		# while(time < 60):
@@ -21,12 +28,14 @@ def simulate(config_file):
 		# 	efforts_caller(joint_efforts)
 
 		# gazebo_exit()
+	return SimRunResponse(100)
 
+def with_generator_server():
+	rospy.init_node('simulator', anonymous=True)	
+	s = rospy.Service('sim_run', SimRun, simulate)
+
+	# spin() keeps Python from exiting until node is shutdown
+	rospy.spin()
 
 if __name__ == '__main__':
-	rospy.init_node('simulator', anonymous=True)	
-
-	local_dir = os.path.dirname(__file__)
-	config_path = os.path.join(local_dir, 'config-feedforward')
-
-	simulate(config_path)
+	with_generator_server()

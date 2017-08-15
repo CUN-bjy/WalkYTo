c6a4a76ae34378e44f1aec1a97f60ce433c5829e
Whitkatana <sinanju06@naver.com>
2017-08-15 22:48:04 +0900

    Add files via upload

---
+++ delete_model_client.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+import rospy
+from gazebo_msgs.srv import *
+
+def delete_model(model_name):
+    rospy.wait_for_service('gazebo/delete_model')
+    
+    try:
+        # create a handle to the add_two_ints service
+        delete_model = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)
+        
+        # simplified style
+        resp1 = delete_model(model_name)
+        print(resp1.success)
+        print(resp1.status_message)
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+if __name__ == "__main__":
+	model_name1 = 'MR_bell_imu'
+	delete_model(model_name1)

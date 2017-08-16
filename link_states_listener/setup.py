911f3dcf7c61d2e9a2f04fa740dd7e0e9664e193
ppaa1135 <30586216+ppaa1135@users.noreply.github.com>
2017-08-16 15:54:02 +0900

    Delete setup.py

--- link_states_listener/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-setup_args = generate_distutils_setup(
-	packages = ['link_states_listener'],
-	packages_dir = {'': 'src'},
-)
-setup(**setup_args)

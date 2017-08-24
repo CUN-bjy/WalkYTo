147a0c87f997cc8f63c519fe2fbe7a3d71decf8e
CUN-bjy <wnsdlqjtm@naver.com>
2017-08-24 15:45:27 +0900

    maybe complete

--- src/network_gen.py
+++ src/network_gen.py
@@ -14,21 +14,20 @@ if not os.path.exists("%s/src/genes"%local_dir):
 def gene_management(genomes):
 	local_dir = os.getenv("WALKYTO_PATH")
 
-	# genome_list = []
-	# genes_list = os.listdir("%s/src/genes"%local_dir)
-	# for genome_id, genome in genomes:
-	# 	genome_list.append(str(genome.key))
+	genome_list = []
+	genes_list = os.listdir("%s/src/genes"%local_dir)
+	for genome_id, genome in genomes:
+		genome_list.append(str(genome.key))
 
-	# s1 = set(genes_list); s2 = set(genome_list)
-	# add_list = [x for x in genome_list if x not in s1]
-	# rm_list = [x for x in genes_list if x not in s2]
+	s1 = set(genes_list); s2 = set(genome_list)
+	add_list = [x for x in genome_list if x not in s1]
+	rm_list = [x for x in genes_list if x not in s2]
 
-	# for rm_file in rm_list:
-	# 	os.remove("%s/src/genes/%s" % (local_dir,rm_file))
+	for rm_file in rm_list:
+		os.remove("%s/src/genes/%s" % (local_dir,rm_file))
 
 	for genome_id, genome in genomes:
-	# 	if(str(genome_id) in add_list):
-		gen_file = open("%s/src/genes/%s" % (local_dir,genome_id),'w')
+		gen_file = open("%s/src/genes/%d" % (local_dir,genome_id),'w')
 		pickle.dump(genome, gen_file)
 
 def gene_id_publisher(gene_string):
@@ -58,18 +57,22 @@ def eval_genomes(genomes, config):
 		gene_list.append(gene)
 
 	dup_num = 4
-	population = 80
+	population = len(id_list)
+	temp_list =list(id_list)
 
 	rate1=rospy.Rate(3)# 10hz
 	rate2=rospy.Rate(5)# 5hz
 	u = 0
-	while(dup_num*(u+1)<=population):
-		print("-----------simulation(%d/%d)--------------"%(dup_num*(u+1), population))
+	while(temp_list != []):
+		gene_string = str(temp_list.pop())
 
-		gene_string = str(id_list[u*dup_num])
 		for i in range(1,dup_num):
-			gene_string = gene_string + '/' + str(id_list[u*dup_num+i])
+			if len(temp_list) > 0:
+				gene_string = gene_string + '/' + str(temp_list.pop())
+			else:
+				break
 
+		print("-----------simulation(%d/%d)--------------"%(population-len(temp_list), population))
 		print("gene_id:", gene_string)
 		gene_id_publisher(gene_string)
 		rate1.sleep()
@@ -82,7 +85,8 @@ def eval_genomes(genomes, config):
 			fitness = fit_caller(fcnt+1)
 			if fitness != -1:
 				fit_list.append(fitness)
-				gene_list[u*dup_num+fcnt].fitness = fitness
+				a_gen = gene_list.pop()
+				a_gen.fitness = fitness
 				fcnt = fcnt + 1
 			else:
 				rate2.sleep()
@@ -94,7 +98,7 @@ def eval_genomes(genomes, config):
 
 
 
-def run(config_file):
+def run(config_file, max_iter):
     # Load configuration.
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
@@ -110,7 +114,7 @@ def run(config_file):
     p.add_reporter(neat.Checkpointer(1))
 
     # Run for up to 300 generations.
-    winner = p.run(eval_genomes, 150)
+    winner = p.run(eval_genomes, max_iter)
 
     # Display the winning genome.
     print('\nBest genome:\n{!s}'.format(winner))
@@ -126,7 +130,17 @@ def run(config_file):
     visualize.plot_species(stats, view=True)
 
 
+def load_checkpoint(config_file, ckp_name, max_iter):
+	# Load configuration.
+	config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+		                 neat.DefaultSpeciesSet, neat.DefaultStagnation,
+		                 config_file)
 
+	# Create the population, which is the top-level object for a NEAT run.
+	p = neat.Population(config)
+
+	p = neat.Checkpointer.restore_checkpoint(ckp_name)
+	p.run(eval_genomes, max_iter)
 
 if __name__ == '__main__':
     # Determine path to configuration file. This path manipulation is
@@ -134,11 +148,16 @@ if __name__ == '__main__':
     # current working directory.
 	# rospy.init_node('network_gen', anonymous=True)	
 	rospy.init_node('network_gen')
+	argv = rospy.myargv()
 	
-	rate1=rospy.Rate(3)# 10hz
-	for i in range(20):
+	rate1=rospy.Rate(2)
+	for i in range(30):
 		gene_id_publisher('-');rate1.sleep()
 
 	local_dir = os.path.dirname(__file__)
 	config_path = os.path.join(local_dir, 'config-feedforward')
-	run(config_path)
+	
+	if argv[2] == '-r':
+		run(config_path, argv[1])
+	elif argv[2] == '-l':
+		load_checkpoint(config_path, argv[3],argv[1])
